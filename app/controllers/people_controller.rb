5426a01a5573a2bf88f893b1f3fa7b51d3dbbda7
lz <li798165486@gmail.com>
2018-04-16 16:34:27 +0800

    change some response data

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -27,6 +27,8 @@ class PeopleController < ApplicationController
 
     @person = Person.new(person_params)
 
+
+
     respond_to do |format|
       if @person.save
         format.html { redirect_to @person, notice: 'Person was successfully created.' }
@@ -66,7 +68,7 @@ class PeopleController < ApplicationController
     p params
     respond_to do |format|
         if Person.update(params["id"], :phone=> params["phone"], :address=>params["address"] ,:name=>params["name"])
-          format.json {render json: {'status'=>"0",'data'=>"ok"} }
+          format.json {render json: {'status'=>"0",'data'=> Person.find(request.query_parameters["id"])} }
         else
           #format.json {render json: json_str.to_json }#如果用json字符串，则需要通过to_json转化格式返回给前台js，那么前台需要执行eval来再次转换格式  
           format.json { render json: a.errors, status: :unprocessable_entity } #此处建议使用hash格式数据返回，这样前台js就无需执行eval来转换格式
@@ -81,7 +83,7 @@ class PeopleController < ApplicationController
       a.name = params["name"]
       respond_to do |format|
           if a.save
-            format.json {render json: {'status'=>"0",'data'=>"ok"} }
+            format.json {render json: {'status'=>"0",'data'=> Person.last} }
           else
             #format.json {render json: json_str.to_json }#如果用json字符串，则需要通过to_json转化格式返回给前台js，那么前台需要执行eval来再次转换格式  
             format.json { render json: a.errors, status: :unprocessable_entity } #此处建议使用hash格式数据返回，这样前台js就无需执行eval来转换格式
@@ -94,7 +96,7 @@ class PeopleController < ApplicationController
       @people = Person.all
       respond_to do |format|
         format.html { render json: {'status'=>"0",'data'=>"ok"} }
-        format.json { render json: {'status'=>"0",'data'=>@people}  }
+        format.json { render json: @people  }
       end 
   end 
 
@@ -103,8 +105,10 @@ class PeopleController < ApplicationController
       @person = Person.find(request.query_parameters["id"])
       respond_to do |format|
         format.html { redirect_to people_url, notice: 'Person was successfully destroyed.' }
-        format.json { render json: {'status'=>"0",'data'=>@person}  }
+        # format.json { render json: {'status'=>"0",'data'=>@person}  }
+        format.json { render json: @person }
       end 
+      
   end 
 
   def remove
