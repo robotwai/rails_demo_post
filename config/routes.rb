4deedd29fcba756233117b2cf00b9b8944d7c45a
lz <li798165486@gmail.com>
2018-04-19 16:50:20 +0800

    add More test,and optimize layout organization,add some stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  
+  # 安卓接口
   get 'people/get_one_data'
   get 'people/get_data'
-  post 'people/update_data'
-  resources :people
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-  get 'static_pages/json'
   post 'people/save_data'
   post 'people/remove'
+  post 'people/update_data'
+  resources :people
+
+  root 'static_pages#home'
+  # get 'home' to 'static_pages#home'
+
+  get '/help', to: 'static_pages#help'
+
+  get '/about', to: 'static_pages#about'
 
   get '/signup', to: 'users#new'
   resources :users
