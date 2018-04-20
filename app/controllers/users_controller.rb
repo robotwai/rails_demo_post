1a2a9640edb19e644ff1063c01aebb2410f539a7
lz <li798165486@gmail.com>
2018-04-20 17:59:51 +0800

    add some test,update puma

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		#
+  		flash[:success] = "Wellcome to the Sample App"
   		redirect_to @user
   	else
   		render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
 
   get '/signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
