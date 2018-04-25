228d9a25fb90294de1e383d3049df37b646d679d
lz <li798165486@gmail.com>
2018-04-25 16:39:03 +0800

    add user permission,add users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :looged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def new
   	@user = User.new
   end
@@ -17,9 +19,42 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
   private
 
   	def user_params
   		params.require(:user).permit(:name,:email,:password,:password_confirmation)
   	end
+
+    def looged_in_user
+      unless  logged_in?
+        store_location
+        flash[:dagger] = "Please log in"
+        redirect_to login_url
+        
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,4 +38,13 @@ module SessionsHelper
 		cookies.delete(:user_id)
 		cookies.delete(:remember_token)
 	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	def store_location
+		session[:forwarding_url] = request.original_url if request.get?
+	end
 end
