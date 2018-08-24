3ecc2cf57a5eee25ff1ed5be30a11a0a958f561e
lz <li798165486@gmail.com>
2018-08-24 17:07:00 +0800

    fix user model bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 	before_save {self.email  = email.downcase}
 	before_create :create_activation_digest
 	validates :name,presence: true,length: {maximum: 20}
-	validates :sign_content,presence: true,length: {maximum: 50}
+	validates :sign_content,length: {maximum: 50}
 	VALID_EMAIL_REGEX  = /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email,presence: true,length: {maximum: 255},
 					 format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false}
